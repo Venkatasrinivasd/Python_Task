4e31c085ea6dd109d7deef34138e563c816f4295
Venkatasrinivasd <113501215+Venkatasrinivasd@users.noreply.github.com>
2022-09-18 11:44:10 +0530

    Add files via upload

---
+++ firstgo_18_09_2022.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	var student1 string = "John" //type is string
+	var student2 = "Jane"        //type is inferred
+	x := 2                       //type is inferred
+	y := 4
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x + y) // addition
+	fmt.Println(x - y) // sub
+	fmt.Println(x * y) // mul
+	fmt.Println(x / y) // div
+	fmt.Println(x % y) // mod
+	x++                // unary operator
+	fmt.Println(x)
+	y--
+	fmt.Println(y)
+
+}
